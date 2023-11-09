8b8a960cbaf387a1a647930df29c77880bb87583
Sebastien Destannes <sdestann@c2r6p9.42perpignan.fr>
2023-11-09 05:41:34 +0100

    essai de suivre le tuto nestJS 4h en video mais avec docker ca complique beaucoup les choses

---
+++ srcs/back/src/auth/auth.services.ts
@@ -0,0 +1,12 @@
+import { Injectable }  from '@nestjs/common';
+
+@Injectable({})
+export class AuthService{
+	signin () {
+		return { msg: 'I\'m signing in' };
+	}
+
+	signup () {
+		return { msg: 'I\'m signing up' };
+	}
+}
