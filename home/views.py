6c7a9342c4594c9776da77e00d700bc5dde0b633
lolefevr <lolefevr@student.42perpignan.fr>
2024-01-29 13:43:58 +0100

    pullMain1

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html')
+	show_howtoplay = True
+	return render(request, 'home.html', {'show_howtoplay': show_howtoplay})
