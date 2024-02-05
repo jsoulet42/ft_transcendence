7f6b4992171a5bdc45f6f4dc8411506a2546ac50
Leilou1 <eudeline.leilou21@gmail.com>
2024-02-05 18:10:38 +0100

    merge

--- tournaments_stats/views.py
+++ tournaments_stats/views.py
@@ -1,7 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
 def tournaments_stats(request):
 	if request.META.get('HTTP_HX_REQUEST'):
 		return render(request, 'tournaments_stats_block.html')
 	return render(request, 'tournaments_stats.html')
+
+def tournaments_stats(request):
+    string_array= ['win', 'lost', 'equality']
+    return render(request, 'tournaments_stats.html', {'string_array': string_array})
