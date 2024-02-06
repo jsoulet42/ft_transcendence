f290a9da61b3441c9b89fcf15f02b6e85c829c73
Leilou1 <eudeline.leilou21@gmail.com>
2024-02-06 17:38:18 +0100

    tableau

--- tournaments_stats/views.py
+++ tournaments_stats/views.py
@@ -5,7 +5,3 @@ def tournaments_stats(request):
 	if request.META.get('HTTP_HX_REQUEST'):
 		return render(request, 'tournaments_stats_block.html')
 	return render(request, 'tournaments_stats.html')
-
-def tournaments_stats(request):
-    string_array= ['win', 'lost', 'equality']
-    return render(request, 'tournaments_stats.html', {'string_array': string_array})
