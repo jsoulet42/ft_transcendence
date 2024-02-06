50a64d7076b51faabc7179834fda41c9efd4d571
Leilou1 <eudeline.leilou21@gmail.com>
2024-02-06 15:16:10 +0100

    oskour

--- home/views.py
+++ home/views.py
@@ -5,9 +5,4 @@ def home(request):
 	show_howtoplay = True
 	return render(request, 'home.html', {'show_howtoplay': show_howtoplay})
 
-def pong(request):
-	if 'HTTP_HX_REQUEST' in request.META:
-		return render(request, 'pong_block.html', {'hx_push_url': '/pong'})
-	else:
-		return render(request, 'pong.html')
 
--- pong/views.py
+++ pong/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def pong(request):
 	if request.META.get('HTTP_HX_REQUEST'):
 		return render(request, 'pong_block.html')
-	return render(request, 'pong/pong.html')
+	return render(request, 'pong.html')
 
 def game(request):
 	if request.META.get('HTTP_HX_REQUEST'):
