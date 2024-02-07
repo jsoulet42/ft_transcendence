d9523c24ad7697e4774295926fafd7bcc589b7f1
mdiamant42 <mdiamant42.student.42perpignan.fr>
2024-02-07 16:18:56 +0100

    intégration des requettes entre django et js

--- pong/views.py
+++ pong/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def pong(request):
 	if request.META.get('HTTP_HX_REQUEST'):
@@ -9,3 +12,26 @@ def game(request):
 	if request.META.get('HTTP_HX_REQUEST'):
 		return render(request, 'game_block.html')
 	return render(request, 'game.html')
+
+@csrf_exempt
+def pongDjango(request):
+	print(request.POST)
+	function1()
+	a = request.POST.get("p1")
+	b = request.POST.get("p2")
+	if a is None:
+		a = 0
+	if b is None:
+		b = 0
+	result = int(a) + int(b)
+	return JsonResponse({"operation_result": result})
+
+
+def function1():
+	print("Django sa mere")
+	# def compute(request):
+	# 	a = request.POST.get("a")
+	# 	b = request.POST.get("b")
+	# 	result = int(a) + int(b)
+	# 	return JsonResponse({"operation_result": result})
+	return 1
