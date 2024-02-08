b37586f88be6cc1c6be00a07bf949d24e4092c8b
jsoulet42 <illidan888@hotmail.fr>
2024-02-08 16:43:56 +0100

    backend in work

--- authentication/views.py
+++ authentication/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render, redirect
 from django.utils.http import urlencode
 from django.utils.crypto import get_random_string
 from django.core.exceptions import PermissionDenied
-
+from django.http import HttpResponse
 from transcendence import settings
 from hub.urls import hub
 from .models import RequestCache
+from backend.models import User, UsersList
+
+def login_required(view_func):
+	def wrapper(request, *args, **kwargs):
+		if not request.user.is_authenticated:
+			return redirect('login')  # Redirige vers la page de connexion si l'utilisateur n'est pas connecté
+		return view_func(request, *args, **kwargs)
+
+	return wrapper
 
 def login(request):
 	if request.META.get('HTTP_HX_REQUEST'):
@@ -51,9 +60,44 @@ def authenticate(request):
 
     if response.status_code // 100 != 2:
         return HttpResponse(status = 500)
-    #store_token_user(request, response.json().get('access_token'))
+    store_token_user(request, response.json().get('access_token'))
     return redirect('hub')
 
 def store_token_user(request, access_token):
 
-    print(access_token)
+	print(access_token)
+	response = requests.get(
+		settings.EXTERNAL_API_USER_URL,
+		headers = {
+			'Authorization': 'Bearer ' + access_token,
+		})
+	if response.status_code // 100 != 2:
+		return None
+	json_response = response.json()
+
+	user_id = json_response.get('id')
+	user_login = json_response.get('login')
+
+	campus_id = json_response.get('campus')[0].get('id')
+	campus_name = json_response.get('campus')[0].get('name')
+
+	campus = UsersList.objects.filter(name = campus_name)
+	user = User.objects.filter(name = user_login)
+
+	if not campus.exists():
+		newlist = UsersList(name = campus_name)
+		newlist.save()
+		campus = newlist
+
+	if not user.exists():
+		newuser = User(
+			name = json_response.get('login'),
+			list = campus
+		)
+		newuser.save()
+		user = newuser
+
+	user.list = campus
+	user.profile_img = json_response.get('image').get('link')
+	user.save()
+	return response.json()
