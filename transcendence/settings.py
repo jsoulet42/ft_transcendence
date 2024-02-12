3c0625299bc701e5d54046168d01f4b595b438cf
jsoulet42 <illidan888@hotmail.fr>
2024-02-12 15:04:15 +0100

    temp

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django_htmx.middleware.HtmxMiddleware',
+	'authentication.middleware.CustomLoginMiddleware',
 ]
 
 ROOT_URLCONF = 'transcendence.urls'
