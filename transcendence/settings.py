99e7dc41062630195038ab935e834fbe191a64ef
Hnogared <hnogared@student.42perpignan.fr>
2024-02-12 10:06:28 +0100

    custom user is now the default django user

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django_htmx.middleware.HtmxMiddleware',
-	'authentication.middleware.CustomUserSessionMiddleware',
 ]
 
 ROOT_URLCONF = 'transcendence.urls'
