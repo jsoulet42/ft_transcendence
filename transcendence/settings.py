4bac5b5edcc07cc21803e8e921673533f44b5f92
Hnogared <133124217+Hnogared@users.noreply.github.com>
2024-02-12 14:41:58 +0100

    temporary fix of the authentication >> TODO customize the middleware to return an anonymous user if not logged in

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django_htmx.middleware.HtmxMiddleware',
+	'authentication.middleware.UserSessionMiddleware',
 ]
 
 ROOT_URLCONF = 'transcendence.urls'
