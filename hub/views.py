2f379363d7b3a1084d1a445fb6d4c1b86021ad08
jsoulet42 <illidan888@hotmail.fr>
2024-02-15 19:10:20 +0100

    patch jsoulet hippo

--- hub/views.py
+++ hub/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from backend.models import CustomUser
 
 # Create your views here.
 def hub(request):
-	return render(request, 'hub.html')
+	devs = CustomUser.objects.filter(is_dev = True)
+	if request.META.get('HTTP_HX_REQUEST'):
+		return render(request, 'hub_block.html', {'devs': devs})
+	return render(request, 'hub.html', {'devs': devs})
