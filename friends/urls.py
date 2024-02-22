564e743c3aee409cfb3939a95bbebf7f6544b9b2
Hnogared <133124217+Hnogared@users.noreply.github.com>
2024-02-22 03:28:59 +0100

    moved friend dropmenu js in a separate file || added friend removal

--- friends/urls.py
+++ friends/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import get_friend_requests, send_friend_request, accept_friend_request, reject_friend_request
+from .views import (
+	remove_friend,
+	get_friend_list,
+	get_friend_requests,
+	send_friend_request,
+	accept_friend_request,
+	reject_friend_request
+)
 
 urlpatterns = [
+	path('remove/', remove_friend),
+	path('list/get/', get_friend_list),
+	path('list/get/<str:username>/', get_friend_list),
 	path('requests/get/', get_friend_requests, name='get_friend_requests'),
 	path('requests/send/', send_friend_request, name='send_friend_request'),
 	path('requests/accept/<str:sender_username>/<str:receiver_username>/', accept_friend_request),
--- friends/views.py
+++ friends/views.py
@@ -5,19 +5,54 @@ from django.http import JsonResponse
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, require_GET
 
 from .models import FriendRequest
 from backend.models import CustomUser
 
 
 @login_required
-def get_friend_requests(request):
-	friend_requests = FriendRequest.objects.filter(receiver=request.user)
-	data = [{'username': request.sender.username} for request in friend_requests]
+@require_GET
+def get_friend_list(request, username=None):
+	if username != None:
+		try:
+			user = CustomUser.objects.get(username=username)
+		except ObjectDoesNotExist:
+			return JsonResponse({'error': 'Could not find user'})
+	else:
+		user = request.user
+
+	data = [{'username': friend.username} for friend in user.friends.all()]
 	return JsonResponse(data, safe=False)
 
 
 @login_required
+@require_GET
+def get_friend_requests(request):
+	friend_requests = FriendRequest.objects.filter(receiver=request.user)
+	data = [{'username': friend_request.sender.username} for friend_request in friend_requests]
+	return JsonResponse(data, safe=False)
+
+
+@login_required
+@require_POST
+def remove_friend(request):
+	friend_username = request.POST.get('friend_username')
+	if not friend_username:
+		return JsonResponse({'error': 'Invalid username data'})
+
+	try:
+		user = request.user
+		friend = user.friends.get(username=friend_username)
+		user.friends.remove(friend)
+		user.save()
+		return JsonResponse({'success': 'Friend removed successfully'})
+	except ObjectDoesNotExist:
+		return JsonResponse({'error': 'Couldn\'t remove friend'})
+
+
+@login_required
+@require_POST
 def send_friend_request(request):
 	receiver = None
 	
@@ -47,6 +82,7 @@ def send_friend_request(request):
 
 
 @login_required
+@require_POST
 def accept_friend_request(request, sender_username=None, receiver_username=None):
 	if sender_username == None:
 		return JsonResponse({'error': 'Invalid sender username'})
@@ -70,6 +106,7 @@ def accept_friend_request(request, sender_username=None, receiver_username=None)
 
 
 @login_required
+@require_POST
 def reject_friend_request(request, sender_username=None, receiver_username=None):
 	if sender_username == None:
 		return JsonResponse({'error': 'Invalid sender username'})
