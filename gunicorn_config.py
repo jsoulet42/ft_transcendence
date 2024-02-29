42931c388d6fedbfa808110cc15a46bd2c73ded1
jsoulet42 <illidan888@hotmail.fr>
2024-02-29 19:54:53 +0000

    readded everything with the new website

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-bind = "0.0.0.0:8000"
-module = "transcendence.wsgi:application"
-
-workers = 4  # Adjust based on your server's resources
-worker_connections = 1000
-threads = 4
-
-certfile = "/etc/letsencrypt/live/dreamteampong.pro/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/dreamteampong.pro/privkey.pem"
