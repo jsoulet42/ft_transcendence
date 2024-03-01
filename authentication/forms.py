b2d5f871b9e699c8ad874f2814374bc328431c93
Leilou1 <eudeline.leilou21@gmail.com>
2024-03-01 18:44:48 +0100

    sign up

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,6 +5,13 @@ from django.conf import settings
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
+    
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
