05697faec6a9fdde02edc2dba3c79ac07b087d53
Hnogared <133124217+Hnogared@users.noreply.github.com>
2024-03-01 16:40:50 +0100

    added old form commented

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -18,3 +18,28 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+# from django import forms
+# from backend.models import CustomUser
+# 
+# class CustomUserCreationForm(forms.ModelForm):
+	# password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+	# password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+# 
+	# class Meta:
+		# model = CustomUser
+		# fields = ['username', 'email']
+# 
+	# def clean_password2(self):
+		# password1 = self.cleaned_data.get("password1")
+		# password2 = self.cleaned_data.get("password2")
+		# if password1 and password2 and password1 != password2:
+			# raise forms.ValidationError("Passwords don't match")
+		# return password2
+# 
+	# def save(self, commit=True):
+		# user = super().save(commit=False)
+		# user.set_password(self.cleaned_data["password1"])
+		# if commit:
+			# user.save()
+		# return user
