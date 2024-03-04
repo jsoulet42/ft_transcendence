e854ab82bcf78335d7604773fd22250717045e2d
jsoulet42 <illidan888@hotmail.fr>
2024-03-04 10:39:18 +0100

    Supprimer tous les fichiers de la branche principale

--- friends/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FriendRequest
-
-
-@admin.register(FriendRequest)
-class FriendRequestAdmin(admin.ModelAdmin):
-    list_display = ("sender", "receiver", "status", "created_at")
-    search_fields = ("sender", "receiver", "status")
--- profile/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from backend.models import CustomUser
-
-
-class ProfilePicForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ('upload_image',)
