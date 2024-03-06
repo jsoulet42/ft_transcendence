9d0aca99c1625a2922a1d74df0940b77311f0701
mdiamant42 <mdiamant42.student.42perpignan.fr>
2024-03-06 16:57:34 +0100

    power-up en cour bug d'initialisation de l'ui dupong a réglé

--- app/pong/static/pong/js/pong.js
+++ app/pong/static/pong/js/pong.js
@@ -73,13 +73,13 @@ function createManager(mode = 0) {
 }
 function createPaddle() {
 	return {
-		leftHeight: canvas.width / 10,
+		leftHeight: canvas.width / 10 * window.sizePaddle / window.sizePaddleBase,
 		leftWidth: canvas.height / 50,
-		rightHeight: canvas.width / 10,
+		rightHeight: canvas.width / 10 * window.sizePaddle / window.sizePaddleBase,
 		rightWidth: canvas.height / 50,
-		leftY: (canvas.height - canvas.width / 10) / 2, // start in the middle of the canvas
-		rightY: (canvas.height - canvas.width / 10) / 2, // start in the middle of the canvas
-		speed: canvas.height / 80,	// Vitesse de déplacement des raquettes
+		leftY: (canvas.height - canvas.width / 10 * window.sizePaddle / window.sizePaddleBase) / 2, // start in the middle of the canvas
+		rightY: (canvas.height - canvas.width / 10 * window.sizePaddle / window.sizePaddleBase) / 2, // start in the middle of the canvas
+		speed: canvas.height / 80 * window.speedPaddle / window.speedPaddleBase,	// Vitesse de déplacement des raquettes
 		centreR: 0,
 		centreL: 0,
 		marge: 10,
@@ -93,7 +93,7 @@ function createBall(speed) {
 		speedX: speed,
 		speedY: 0,
 		Bradius: canvas.height / 50,
-		speedBaseX: canvas.width / 100 * 1, // Vitesse de déplacement horizontal de la balle
+		speedBaseX: canvas.width / 100 * window.speed / window.speedBallBase, // Vitesse de déplacement horizontal de la balle
 		speedBaseY: canvas.height / 100 * 0, // Vitesse de déplacement vertical de la balle
 		Bcolor: 'blue'
 	};
@@ -598,7 +598,26 @@ function endGame() {
 	}
 	initializeIA(true);
 	startUpdatingAI();
+	reinitializeSettings();
 }
+
+function reinitializeSettings() {
+	window.speedBallBase = 5;
+	window.sizePaddleBase = 5;
+	window.speedPaddleBase = 5;
+
+	window.speed = window.speedBallBase;
+	window.sizePaddle = window.sizePaddleBase;
+	window.speedPaddle = window.speedPaddleBase;
+
+
+
+	document.getElementById('speedBall').value = window.speedBallBase;
+	document.getElementById('sizePaddle').value = window.sizePaddleBase;
+	document.getElementById('speedPaddle').value = window.speedPaddleBase;
+}
+
+
 async function putBackBall(directionX) {
 	manager.putBackBallBool = true;
 	IA.destYLeft = canvas.height / 2;
@@ -975,7 +994,6 @@ function sendTournamentScoreToBackend() {
 
 //#endregion
 
-
 //#region PowerUp
 
 /*
@@ -997,9 +1015,6 @@ Ce module vise à donner aux utilisateurs la flexibilité d’ajuster leur expé
 de personnalisation, tout en offrant aussi une version par défaut, simple, pour les
 utilisateurs qui désirent ce type d’expérience.
 */
-
-
-
 
 
 //endregion
