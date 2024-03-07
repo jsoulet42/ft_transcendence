d8e55c58f6e8d2f94bc419cddd9ecb30cc0a0423
jsoulet42 <illidan888@hotmail.fr>
2024-03-07 19:13:08 +0100

    fixed form errors

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -8,10 +8,27 @@ from django.utils.translation import gettext as _
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control form_signup1', 'placeholder': _('Username')})
-        self.fields['email'].widget.attrs.update({'class': 'form-control form_signup2', 'placeholder': _('Email (optional)')})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control form_signup3', 'placeholder': _('Password')})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control form_signup4', 'placeholder': _('Confirm Password')})
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control form_signup1',
+            'autocomplete': 'username',
+            'placeholder': _('Username')})
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control form_signup2',
+            'autocomplete': 'email',
+            'placeholder': _('Email (optional)')})
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control form_signup3',
+            'autocomplete': 'current-password',
+            'placeholder': _('Password')})
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control form_signup4',
+            'autocomplete': 'current-password',
+            'placeholder': _('Confirm Password')})
+
+        self.fields['username'].label = _("Username")
+        self.fields['email'].label = _("Email")
+        self.fields['password1'].label = _("Password")
+        self.fields['password2'].label = _("Confirmation Password")
 
     class Meta:
         model = CustomUser
@@ -25,7 +42,8 @@ class CustomUserCreationForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
-        list_instance, created = UsersList.objects.get_or_create(name='No42User')
+        list_instance, created = UsersList.objects.get_or_create(
+            name='No42User')
         image_url = settings.MEDIA_URL + 'character2.png'
         user.list = list_instance
         user.profile_image_path = image_url
@@ -33,28 +51,3 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-# from django import forms
-# from backend.models import CustomUser
-#
-# class CustomUserCreationForm(forms.ModelForm):
-	# password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-	# password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-#
-	# class Meta:
-		# model = CustomUser
-		# fields = ['username', 'email']
-#
-	# def clean_password2(self):
-		# password1 = self.cleaned_data.get("password1")
-		# password2 = self.cleaned_data.get("password2")
-		# if password1 and password2 and password1 != password2:
-			# raise forms.ValidationError("Passwords don't match")
-		# return password2
-#
-	# def save(self, commit=True):
-		# user = super().save(commit=False)
-		# user.set_password(self.cleaned_data["password1"])
-		# if commit:
-			# user.save()
-		# return user
