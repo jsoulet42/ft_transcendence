7e61d1e3f7453dbe67805b0b9a62f3194f7bb7fa
Hnogared <133124217+Hnogared@users.noreply.github.com>
2024-03-07 08:05:54 +0100

    trying to cleanup all the head scripts || added language selection dropdown

--- app/transcendence/urls.py
+++ app/transcendence/urls.py
@@ -21,9 +21,11 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.views.i18n import JavaScriptCatalog
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+] + i18n_patterns(
     path('', include('hub.urls')),
-	path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('admin/', admin.site.urls),
     path('backend/', include('backend.urls')),
     path('home/', include('home.urls')),
